7b0a64fddd9ca0d62d90542ffa02fdd3c83fa233
Yago Henrique <yagoh686@gmail.com>
2025-06-25 14:39:31 -0300

    alteração

--- controllers/LogoutController.js
+++ controllers/LogoutController.js
@@ -1,31 +1,11 @@
-const LogoutService = require('../services/LogoutService');
+module.exports = {
+  logout: async (req, res) => {
+    const { id } = req.body;
 
-class LogoutController{
-    static async logout(req,res){
-        const { id } = req.body;
+    if (!id) {
+      return res.status(400).json({ mensagem: 'ID do usuário não fornecido.' });
+    }
 
-        if (!id) {
-            return res.status(400).json({
-                mensagem: 'ID do usuário não fornecido.' });
-        }
-        try {
-            const usuarioExiste = await LogoutService.logout(id);
-
-            if (!usuarioExiste) {
-                return res.status(404).json({
-                    mensagem: 'Usuário não encontrado.'});
-            }
-
-        
-        return res.status(200).json({
-            mensagem: 'Logout realizado com sucesso.'});
-        } catch (error) {
-            return res.status(500).json({ mensagem: 'Erro ao realizar logout.' });
-        };
-
-    };
-
+    return res.status(200).json({ mensagem: 'Logout simulado com sucesso.' });
+  }
 };
-
-module.exports = LogoutController;
-
